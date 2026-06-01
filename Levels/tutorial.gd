f52dcd863a1f22e0f58637c6a7da63c0ca44143b
Abbby1007 <Abigailloken25@gmail.com>
2026-06-02 08:12:31 +0900

    Basic script where the platforms spawn in

--- Levels/tutorial.gd
+++ Levels/tutorial.gd
@@ -1,15 +1,20 @@
 extends Node2D
 
-var platform = preload("res://kill_platform.tscn")
-# Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#randomize()
-	#var y = 0
-	#while y > - 3000:
-		#var new_platforms = platform.instantiate()
-		##new_platforms.set_pos(Vector2(0,y))
-		#add_child(new_platforms)
-		#y -= randf_range(0,210)
+@onready var platform_container := $platform_container
+@onready var  platform_initial_position_y = $platform_container/killPlatform.position.y
+
+@export var platform_scene: PackedScene
+#45 - 357
+func level_generator(amount):
+	for items in amount:
+		platform_initial_position_y -= randf_range(36,54)
+		var new_platform = platform_scene.instantiate() as StaticBody2D
+		new_platform.position = Vector2(randf_range(45,357),platform_initial_position_y)
+		platform_container.add_child(new_platform)
+	print(amount)
+
+func _ready() -> void:
+	level_generator(20)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
